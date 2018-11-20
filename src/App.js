c0f76ca70228d95f1bd117957878311e42d03024
Hope R <nadezhda.lkn@yahoo.com>
2018-11-20 17:48:34 +0300

    Apply styles to components with css-in-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import MobileScreen from './components/MobileScreen/MobileScreen.js'
+
+const AppDiv = styled.div`
+    text-align: center;
+`
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>I'm a mobile phone</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <AppDiv>
+                <h1>I'm a mobile phone</h1>
+                <MobileScreen />
+            </AppDiv>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/MobileScreen/MobileScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+width: 350px;
+height: 500px;
+margin: auto; 
+    border: 1px solid #858585;
+
+
+    &:hover {
+        box-shadow: 2px 2px 2px 0 #333333;
+    }
+`
+
+const MobileScreen = () => {
+    return <Container />
+}
+
+export default MobileScreen
