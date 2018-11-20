11ed9bd2300f22ecb0dcb755b4d095bc2c40040c
Hope R <nadezhda.lkn@yahoo.com>
2018-11-20 17:56:10 +0300

    Fix styles a bit

--- src/components/MobileScreen/MobileScreen.js
+++ src/components/MobileScreen/MobileScreen.js
@@ -2,14 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-width: 350px;
+width: 320px;
 height: 500px;
 margin: auto; 
     border: 1px solid #858585;
 
-
     &:hover {
-        box-shadow: 2px 2px 2px 0 #333333;
+        box-shadow: 2px 2px 6px 0 #333333;
     }
 `
 
