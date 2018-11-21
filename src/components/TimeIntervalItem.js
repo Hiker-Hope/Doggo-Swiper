822360d4dca53a98578b3f41b60a66e56ff0df23
Hope R <nadezhda.lkn@yahoo.com>
2018-11-21 16:32:49 +0300

    Remove @keyframes from css file

--- src/components/TimeIntervalItem.js
+++ src/components/TimeIntervalItem.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import './animation.css'
 
+const indicator = keyframes`
+    0% {
+        background-position: right bottom;
+
+    }
+    100% {
+        background-position: left bottom;
+
+    }
+`
 const Container = styled.div`
     margin: 0 5px;
     flex: 1;
@@ -9,7 +19,7 @@ const Container = styled.div`
     height: 7px;
     background: linear-gradient(to left, gray 50%, orange 50%);
     background-size: 200% 100%;
-    animation-name: indicator;
+    animation-name: ${indicator};
     animation-duration: 2s;
 `
 
